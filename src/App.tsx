811bcd81734a27cf48bd121fe17d224a4814659f
Heba <hebahiary+github@gmail.com>
2023-11-01 19:17:12 -0700

    questions fetched from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,74 @@
 import React, { useState } from "react";
+import { fetchQuizQuestions } from "./API";
+//Components
+import QuestionCard from "./components/QuestionCard";
+
+//Types
+import { Difficulty, QuestionState } from "./API";
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
-  return <h1>App</h1>;
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.HARD
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setCurrentQuestionNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>React Quiz</h1>
+      {gameOver || userAnswers.length == TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+
+      {!gameOver ? <p className="score">Score:</p> : null}
+      {loading ? <p>Loading Questions ... </p> : null}
+      {/* <QuestionCard
+        questionNumber={currentQuestionNumber + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[currentQuestionNumber].question}
+        answers={questions[currentQuestionNumber].answers}
+        userAnswer={
+          userAnswers ? userAnswers[currentQuestionNumber] : undefined
+        }
+        callback={checkAnswer}
+      /> */}
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/utils.ts
@@ -0,0 +1,2 @@
+export const shuffleArray = (array: any[]) => 
+{ return [...array].sort(() => Math.random() - 0.5)}
