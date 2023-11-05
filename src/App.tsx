6c63b19fb8668270bb48547c327844ba297cc18e
Heba <hebahiary+github@gmail.com>
2023-11-05 07:09:17 -0800

    Revert "style changes"
    This reverts commit b7cf51dc083b41af9ce30a9c602dc710ae10ee64.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { fetchQuizQuestions } from "./API";
 //Components
 import QuestionCard from "./components/QuestionCard";
+
 //Types
 import { Difficulty, QuestionState } from "./API";
 
@@ -12,10 +13,11 @@ export type AnswerObject = {
   correctAnswer: string;
 };
 
+const TOTAL_QUESTIONS = 10;
+const DIFFICULTY = Difficulty.EASY;
+
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [difficulty, setDifficulty] = useState(Difficulty.EASY)
-  const [totalQuestions, setTotalQuestions] = useState(10)
   const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
@@ -26,7 +28,7 @@ const App = () => {
     setLoading(true);
     setGameOver(false);
 
-    const newQuestions = await fetchQuizQuestions(totalQuestions, difficulty);
+    const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS, DIFFICULTY);
 
     setQuestions(newQuestions);
     setScore(0);
@@ -57,7 +59,7 @@ const App = () => {
   const nextQuestion = () => {
     const nextQuestionNumber = currentQuestionNumber + 1;
 
-    if (nextQuestionNumber === totalQuestions) {
+    if (nextQuestionNumber === TOTAL_QUESTIONS) {
       setGameOver(true);
     } else {
       setCurrentQuestionNumber(nextQuestionNumber);
@@ -65,23 +67,20 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col	justify-end items-center gap-2">
-      <h1 className="text-3xl font-bold">Trivia</h1>
+    <div className="App">
+      <h1>React Quiz</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
 
-{gameOver && (
-  <div>
-    <p>Select Difficulty:</p>
-    <button onClick={() => {setDifficulty(Difficulty.EASY)}}>Easy</button>
-    <button onClick={() => {setDifficulty(Difficulty.MEDIUM)}}>Meduim</button>
-    <button onClick={() => {setDifficulty(Difficulty.HARD)}}>Hard</button>
-  </div>
-)}
-
+      {!gameOver ? <p className="score">Score: {score}</p> : null}
       {loading && <p>Loading Questions ... </p>}
       {!loading && !gameOver && (
         <QuestionCard
           questionNumber={currentQuestionNumber + 1}
-          totalQuestions={totalQuestions}
+          totalQuestions={TOTAL_QUESTIONS}
           question={questions[currentQuestionNumber].question}
           answers={questions[currentQuestionNumber].answers}
           userAnswer={
@@ -92,18 +91,12 @@ const App = () => {
       )}
       {!loading &&
         !gameOver &&
-        
-        currentQuestionNumber !== totalQuestions - 1 && (
-          <button disabled={userAnswers.length === currentQuestionNumber + 1 ? false : true} className="bg-white hover:bg-gray-100 hover:disabled:bg-white text-gray-800 font-semibold py-2 px-4 border border-gray-400 disabled:border-gray-200 rounded shadow disabled:text-gray-300" onClick={nextQuestion}>
+        userAnswers.length === currentQuestionNumber + 1 &&
+        currentQuestionNumber !== TOTAL_QUESTIONS - 1 && (
+          <button className="next" onClick={nextQuestion}>
             Next Question
           </button>
         )}
-              {userAnswers.length === totalQuestions ? (<><h3>Game Over!</h3><p className="score">Score: {score}</p></>) : null}
-              {gameOver || userAnswers.length === totalQuestions ? (
-        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={startTrivia}>
-          {userAnswers.length === totalQuestions ? "Restart" : "Start"}
-        </button>
-      ) : null}
     </div>
   );
 };
