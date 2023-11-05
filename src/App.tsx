b420695bf8d25d92fa44665e4a83d7d59e9f68dc
Heba <hebahiary+github@gmail.com>
2023-11-05 07:44:07 -0800

    switched styling to MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { fetchQuizQuestions } from "./API";
-//Components
+// Components
 import QuestionCard from "./components/QuestionCard";
-
-//Types
+// Types
 import { Difficulty, QuestionState } from "./API";
+// MUI
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
 export type AnswerObject = {
   question: string;
@@ -13,11 +15,10 @@ export type AnswerObject = {
   correctAnswer: string;
 };
 
-const TOTAL_QUESTIONS = 10;
-const DIFFICULTY = Difficulty.EASY;
-
 const App = () => {
   const [loading, setLoading] = useState(false);
+  const [difficulty, setDifficulty] = useState(Difficulty.EASY);
+  const [totalQuestions, setTotalQuestions] = useState(10);
   const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
@@ -28,7 +29,7 @@ const App = () => {
     setLoading(true);
     setGameOver(false);
 
-    const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS, DIFFICULTY);
+    const newQuestions = await fetchQuizQuestions(totalQuestions, difficulty);
 
     setQuestions(newQuestions);
     setScore(0);
@@ -41,7 +42,7 @@ const App = () => {
     if (!gameOver) {
       // users answer
       const answer = e.currentTarget.value;
-      // check if answer is correct
+      // check if the answer is correct
       const correct =
         questions[currentQuestionNumber].correct_answer === answer;
       if (correct) setScore((prev) => prev + 1);
@@ -59,7 +60,7 @@ const App = () => {
   const nextQuestion = () => {
     const nextQuestionNumber = currentQuestionNumber + 1;
 
-    if (nextQuestionNumber === TOTAL_QUESTIONS) {
+    if (nextQuestionNumber === totalQuestions) {
       setGameOver(true);
     } else {
       setCurrentQuestionNumber(nextQuestionNumber);
@@ -67,36 +68,111 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <h1>React Quiz</h1>
-      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-        <button className="start" onClick={startTrivia}>
-          Start
-        </button>
-      ) : null}
-
-      {!gameOver ? <p className="score">Score: {score}</p> : null}
-      {loading && <p>Loading Questions ... </p>}
-      {!loading && !gameOver && (
-        <QuestionCard
-          questionNumber={currentQuestionNumber + 1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[currentQuestionNumber].question}
-          answers={questions[currentQuestionNumber].answers}
-          userAnswer={
-            userAnswers ? userAnswers[currentQuestionNumber] : undefined
-          }
-          callback={checkAnswer}
-        />
-      )}
-      {!loading &&
-        !gameOver &&
-        userAnswers.length === currentQuestionNumber + 1 &&
-        currentQuestionNumber !== TOTAL_QUESTIONS - 1 && (
-          <button className="next" onClick={nextQuestion}>
-            Next Question
-          </button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <h1>Trivia</h1>
+        {gameOver && (
+          <Grid
+            container
+            spacing={2}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <p>Select Difficulty:</p>
+            <Grid container justifyContent="center" gap={1}>
+              <Button
+                variant="outlined"
+                size="small"
+                style={{
+                  backgroundColor:
+                    difficulty === Difficulty.EASY ? "azure" : "white",
+                }}
+                onClick={() => {
+                  setDifficulty(Difficulty.EASY);
+                }}
+              >
+                Easy
+              </Button>
+              <Button
+                variant="outlined"
+                size="small"
+                style={{
+                  backgroundColor:
+                    difficulty === Difficulty.MEDIUM ? "azure" : "white",
+                }}
+                onClick={() => {
+                  setDifficulty(Difficulty.MEDIUM);
+                }}
+              >
+                Medium
+              </Button>
+              <Button
+                variant="outlined"
+                size="small"
+                style={{
+                  backgroundColor:
+                    difficulty === Difficulty.HARD ? "azure" : "white",
+                }}
+                onClick={() => {
+                  setDifficulty(Difficulty.HARD);
+                }}
+              >
+                Hard
+              </Button>
+            </Grid>
+          </Grid>
         )}
+        {loading && <p>Loading Questions ... </p>}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNumber={currentQuestionNumber + 1}
+            totalQuestions={totalQuestions}
+            question={questions[currentQuestionNumber].question}
+            answers={questions[currentQuestionNumber].answers}
+            userAnswer={
+              userAnswers ? userAnswers[currentQuestionNumber] : undefined
+            }
+            callback={checkAnswer}
+          />
+        )}
+        {!loading &&
+          !gameOver &&
+          currentQuestionNumber !== totalQuestions - 1 && (
+            <Button
+              disabled={
+                userAnswers.length === currentQuestionNumber + 1 ? false : true
+              }
+              onClick={nextQuestion}
+            >
+              Next Question
+            </Button>
+          )}
+        {userAnswers.length === totalQuestions ? (
+          <>
+            <h3>Game Over!</h3>
+            <p className="score">Score: {score}</p>
+          </>
+        ) : null}
+        {gameOver || userAnswers.length === totalQuestions ? (
+          <Button size="large" variant="contained" onClick={startTrivia}>
+            {userAnswers.length === totalQuestions ? "Restart" : "Start"}
+          </Button>
+        ) : null}
+      </Grid>
     </div>
   );
 };
